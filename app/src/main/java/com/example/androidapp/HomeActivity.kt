06cc51dba9ca4206627f8f59c7934a6f006eadf8
Sundhar Kannan <s64kannan@gmail.com>
2024-06-14 11:34:42 +0530

    Recycler View

--- app/src/main/java/com/example/androidapp/HomeActivity.kt
+++ app/src/main/java/com/example/androidapp/HomeActivity.kt
@@ -7,16 +7,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidapp.kotlinegs.WordsViewAdapter
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var homeTextView: TextView
-
+    lateinit var myRecyclerView: RecyclerView
+    var dataArray= arrayOf("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
 
-        homeTextView = findViewById(R.id.tvWelcomeHome) // Initialize here
+        homeTextView = findViewById(R.id.tvWelcomeHome)
+        myRecyclerView=findViewById(R.id.recyclerView)
+        myRecyclerView.layoutManager = LinearLayoutManager(this)
+        var wordsAdapter = WordsViewAdapter(dataArray)
+        myRecyclerView.adapter = wordsAdapter
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -25,6 +33,7 @@ class HomeActivity : AppCompatActivity() {
         }
 
         val data = intent.extras?.getString("nkey")
-        homeTextView.text = "welcome $data"// Set text after initialization
+        homeTextView.text = "welcome $data"//Welcome the user
     }
+
 }
